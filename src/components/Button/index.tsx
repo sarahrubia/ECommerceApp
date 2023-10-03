cc7aa2d37b136c5f6be2cb3c43c1b872bbfe3e26
sarahrubia <sarahrubia22@gmail.com>
2023-10-03 06:57:05 -0300

    refactor: add containerStyles prop to Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,17 @@
-import {Text, Pressable} from 'react-native';
 import React from 'react';
+import {Text, Pressable} from 'react-native';
+
 import styles from './styles';
 
 type ButtonProps = {
   text: string;
   onPress: () => void;
+  containerStyles?: object;
 };
 
-export default function Button({text, onPress}: ButtonProps) {
+export default function Button({text, onPress, containerStyles}: ButtonProps) {
   return (
-    <Pressable onPress={onPress} style={styles.container}>
+    <Pressable onPress={onPress} style={[styles.container, containerStyles]}>
       <Text style={styles.text}>{text}</Text>
     </Pressable>
   );
